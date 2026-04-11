a671aa92ed68e70a4d47bdcd348e1556dfa1642f
Bunnntyyy <104865101+Buuuntyyy@users.noreply.github.com>
2026-04-11 16:45:53 +0200

    fix: Change the free quota

--- frontend/src/views/landing/PricingView.vue
+++ frontend/src/views/landing/PricingView.vue
@@ -27,7 +27,7 @@
                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                   <polyline points="20 6 9 17 4 12"></polyline>
                 </svg>
-                5 Go de stockage chiffré
+                20 Go de stockage chiffré
               </li>
               <li>
                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
